114f27dc859b38ade753baaab80c824111befa42
Andrew Mathies <awmathie@indiana.edu>
2020-04-12 16:00:08 -0400

    testing cd builds

---
+++ Assets/example.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class example : MonoBehaviour
+{
+
+    private float x, y, z;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	float move = Input.GetAxis("Horizontal");
+	transform.Translate(move * Time.deltaTime * speed, 0, 0);
+    }
+}
